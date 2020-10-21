45c8f42c1c2333734444e99bdbb429362fd6a03f
MedAmineBenyettou <medamine.benyettou@gmail.com>
2020-10-21 20:02:37 +0100

    Forum children fixed

--- src/__tests__/Forum init/forum_init.test.js
+++ src/__tests__/Forum init/forum_init.test.js
@@ -1,18 +1,26 @@
 import { CategoryData } from '../../lib/Category';
 import Forum from '../../lib/Forum';
-
+import { TopicData } from '../../lib/Topic';
 test('Initialises a Forum (No data & No options)', () => {
  const forum = new Forum();
  expect(forum.children.length).toBe(0);
 });
 
 test('Initialises a Forum (WITH data & No options)', () => {
+ /**@type {TopicData} */
+ const t = { type: 'TOPIC', title: 'Test', author: 'moi' },
+  c = { type: 'CATEGORY', name: 'test' };
+
  const f = new Forum({
-  //   children: [{ type: 'TOPIC', title: 'test', author: 'test', isPinned: false }],
   children: [{ type: 'CATEGORY', name: 'test' }],
  });
- var test = f//  var test = f._children;
- .console
-  .log(test);
+ const f2 = new Forum({
+  children: [t, t, t, t, t],
+ });
+
+ var cat = f.children[0];
+
+ var top = f2.children[0];
+
  expect(f.children[0].id).toBeInstanceOf(String);
 });
--- src/lib/Forum.ts
+++ src/lib/Forum.ts
@@ -18,27 +18,43 @@ export interface forumOptions {
  defaultTab: 'CATEGORIES' | 'LATEST' | 'TOP';
 }
 
-export interface forumData<T extends ForumChildrenData> {
- children?: T;
+export type forumData<T extends ForumChildrenData = ForumChildrenData> = {
+ children?: T extends CategoryData[] ? CategoryData[] : TopicData[];
  options?: forumOptions; // TODO add more options
  theme?: string; // TODO Change
-}
+};
+export type forumDataG = {
+ options?: forumOptions; // TODO add more options
+ theme?: string; // TODO Change
+};
+export type forumDataC = {
+ children?: CategoryData[];
+} & forumDataG;
+export type forumDataT = {
+ children?: TopicData[];
+} & forumDataG;
 
-export default class Forum<T extends ForumChildrenData> {
- children: Children = [];
+type test = forumData['children'];
+
+type diff = forumDataC | forumDataT extends forumDataC ? Category : Topic;
+
+export default class Forum<T extends forumData = forumData> {
+ //  children: Array<T extends forumData<CategoryData[]> ? Category : Topic> = [];
+ children: Array<T extends forumData<CategoryData[]> ? Category : Topic> = [];
  options: forumOptions | null = null;
  theme: string | null = null;
 
- constructor(data?: forumData<T>) {
+ constructor(data?: T) {
   if (data !== undefined) {
-   this.children;
+   //    this.children;
    const { children, options, theme } = data;
    if (children !== undefined && children.length > 0) {
     if (isCategoryOrTopicDataArray(children)) {
-     this.children = children.map((c) => new Category(c));
-     //  this.children.push(children.forEach((c) => new Category(c)));
+     (this.children as Category[]) = children.map((c) => new Category(c));
+     //    (this.children as Category[]).push(children.forEach((c) => new Category(c)));
     } else {
-     this.children = (children as Topic[]).map((c) => new Topic(c));
+     //   this.children.push(children.forEach((c) => new Topic(c)));
+     (this.children as Topic[]) = children.map((c) => new Topic(c));
     }
    }
    if (options !== undefined) this.options = options;
@@ -46,17 +62,25 @@ export default class Forum<T extends ForumChildrenData> {
   }
  }
 
+ //  setChildren<S extends ForumChildrenData>(data: forumData<S>):S extends CategoryData[]?Forum<Category[]>:Forum<Topic[]>{
+ //     if(data.children){
+ //         if(data.children[0].type==="CATEGORY"){
+ //             return new Forum<Category>(data);
+ //         }
+ //     }
+ //  }
+
  //  isCategoryOnly(): Children {
  //   if (this.children[0] instanceof Category) return <Category[]>this.children;
  //   else return <Topic[]>this.children;
  //  }
 
- //  get _children() {
- //   if (this.children[0] instanceof Category) return <Category[]>this.children;
- //   else return <Topic[]>this.children;
- //   //   if (this.children[0] instanceof Category) return <Category[]>this.children;
- //   //   return <Topic[]>this.children;
- //  }
+ //   get getChildren() {
+ //    if (this.children[0] instanceof Category) return <Category[]>this.children;
+ //    else return <Topic[]>this.children;
+ //    //   if (this.children[0] instanceof Category) return <Category[]>this.children;
+ //    //   return <Topic[]>this.children;
+ //   }
 
  public addCategory(data: CategoryData) {}
 }
