9a5a22fe1819b88acc1152f55769c72793c158fc
MedAmineBenyettou <medamine.benyettou@gmail.com>
2021-04-22 14:05:15 +0100

    tests update

--- src/__tests__/Forum init/forum_init.test.js
+++ src/__tests__/Forum init/forum_init.test.js
@@ -1,26 +1,8 @@
-import { CategoryData } from '../../lib/Category';
-import Forum from '../../lib/Forum';
-import { TopicData } from '../../lib/Topic';
-test('Initialises a Forum (No data & No options)', () => {
- const forum = new Forum();
- expect(forum.children.length).toBe(0);
-});
+import { rest } from 'msw';
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import Forum from '../../components/Forum';
+import { renderHook } from '@testing-library/react-hooks';
+import { initForum } from '../../contexts/forum/Actions/forum';
+import { useForum } from '../../contexts/forum/ForumContext';
 
-test('Initialises a Forum (WITH data & No options)', () => {
- /**@type {TopicData} */
- const t = { type: 'TOPIC', title: 'Test', author: 'moi' },
-  c = { type: 'CATEGORY', name: 'test' };
-
- const f = new Forum({
-  children: [{ type: 'CATEGORY', name: 'test' }],
- });
- const f2 = new Forum({
-  children: [t, t, t, t, t],
- });
-
- var cat = f.children[0];
-
- var top = f2.children[0];
-
- expect(f.children[0].id).toBeInstanceOf(String);
-});
+test('Initialises the Forum: No api functions', () => {});
